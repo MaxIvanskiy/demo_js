d39a6705445d7aaef0cc08f78a7493caae8cdae0
Max Ivanskyi <m.ivanskiy@gmail.com>
2021-08-25 17:48:25 +0300

    task1 + tests

--- main.js
+++ main.js
@@ -1,4 +1,2 @@
-import { chees } from './src/task1.js';
-
 let x = chees(25, 5, 'x');
 console.log(x);
--- src/task1.js
+++ src/task1.js
@@ -1,12 +1,24 @@
-export const chees = (width, height, symbol) => {
+const chees = (width, height, symbol) => {
     let result = '';
-    
-    for (let i = 0; i < height; i += 1) {
-        for (let j = 0; j < width; j += 1) {
-            result = `${result}${symbol} `;
-        }
-        i % 2 ? result = `${result}\n` : result = `${result}\n `
+    if(width <= 0 || height <= 0){
+        let invalidData = {
+            status : 'failed',
+            reason : 'width and height must be bigger than 0'
+        };
+        return invalidData;
+    }else if(width == undefined || height == undefined || symbol == undefined){
+        let invalidData = {
+            status : 'failed',
+            reason : 'please, put width, length and symbol to the function call'
+        };
+        return invalidData;
+    }else{ 
+        for (let i = 0; i < height; i += 1) {
+            for (let j = 0; j < width; j += 1) {
+                result = `${result}${symbol} `;
+            }
+            i % 2 ? result = `${result}\n` : result = `${result}\n `
+         }
+        return result;
     }
-
-    return result;
 }
