d470283379a563966c1df8f2d78464c1fef5008d
Max Ivanskyi <m.ivanskiy@gmail.com>
2021-08-31 09:34:11 +0300

    task6 + tests

--- src/task6.js
+++ src/task6.js
@@ -1,3 +1,26 @@
-export const task6 = () => {
-    return true;
+let rowLen = 10;
+let minPow = 25;
+
+export const task6 = (len, pow) => {
+    if(len == undefined || pow == undefined){
+        return false;
+    }if(typeof(len) != 'number' || typeof(pow) != 'number'){
+        return false;
+    }else{
+        let result = '';
+        let row = [];
+        let start = 1;
+
+        while(row.length < len){
+            let sq = start * start;
+            if(sq >= pow){
+                row.push(start);
+            }
+            start += 1;
+        }
+        result = row.join(',');
+        
+        return result;
+    }
 }
+task6(rowLen, minPow);
--- test/test6.js
+++ test/test6.js
@@ -2,8 +2,34 @@ import { task6 } from '../src/task6.js';
 
 export const test6 = (assert) => {
     describe('Task 6', () => {
-        it('First cehck', () => {
-            assert.equal(8, 8);
+        it('function work correctly', () => {
+            let rowLen = 10;
+            let minPow = 25;
+            let result = task6(rowLen, minPow);
+            assert.equal(result, '5,6,7,8,9,10,11,12,13,14');
         }); 
+        it('function return String', () => {
+            let rowLen = 10;
+            let minPow = 25;
+            let result = task6(rowLen, minPow);
+            assert.typeOf(result, 'string');
+        });
+        it('result length is equal to expected length', () => {
+            let rowLen = 10;
+            let minPow = 25;
+            let result = task6(rowLen, minPow);
+            let actualLength = result.split(',').length;
+            assert.equal(actualLength, rowLen);
+        });
+        it('function call have arguments', () => {
+            let result = task6();
+            assert.isFalse(result);
+        });
+        it('function call arguments are numbers', () => {
+            let rowLen = [10];
+            let minPow = '25';
+            let result = task6(rowLen, minPow);
+            assert.isFalse(result);
+        });
     });
 }
