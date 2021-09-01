87588ed68906dac71440cb5e0f809bba796504ec
Max Ivanskyi <m.ivanskiy@gmail.com>
2021-09-01 12:13:34 +0300

    task7 + tests

--- src/task7.js
+++ src/task7.js
@@ -1,3 +1,46 @@
-export const task7 = () => {
-    return true;
+let contextMinMax = {
+    min : 10,
+    max : 10000
+};
+let contextLength = {
+    length : 10
+};
+
+export const task7 = (context) => {
+    if(context == undefined){
+        return false;
+    }else if(typeof(context) != 'object' || Array.isArray(context)){
+        return false;
+    }else{
+        let result = [];
+        if( (context.min && context.max) && context.min > 0 && context.max <= 2000000000 ){
+            if(result.length == 0){
+                result.push(0, 1);
+            }      
+            while(result[result.length-1] < context.max){
+                let newEl = result[result.length-1] + result[result.length-2];
+                if(newEl > context.max){
+                    break;
+                }
+                result.push(newEl);
+            }
+            let min = result.findIndex(el => el >= context.min);
+            result.splice(0, min);
+        }else if(context.length && context.length <= 47 && context.length > 0){        
+            while(result.length < context.length){
+                if(result.length == 0){
+                    result.push(0);
+                    result.push(1);
+                }else{
+                    let lastEl = result.length-1;
+                    let lastButOne = result.length-2;
+                    let newEl = result[lastEl] + result[lastButOne];
+                    result.push(newEl);
+                }
+            }
+        }else{
+            result = false;
+        }
+        return result;
+    }
 }
--- test/test7.js
+++ test/test7.js
@@ -2,8 +2,62 @@ import { task7 } from '../src/task7.js';
 
 export const test7 = (assert) => {
     describe('Task 7', () => {
-        it('First Check', () => {
-            assert.equal(8, 8);
+        it('function return array', () => {
+            let context = { length : 10 };
+            let result = task7(context);
+            assert.isArray(result);
+
+            context = { min : 10,max : 10000 };
+            result = task7(context);
+            assert.isArray(result);
         }); 
+        it('function call have argument', () => {
+            let result = task7();
+            assert.isFalse(result);
+        });
+        it('function call argument is Object', () => {
+            let context = 123;
+            let result = task7(context);
+            assert.isFalse(result);
+
+            context = [1, 2, 3];
+            result = task7(context);
+            assert.isFalse(result);
+
+            context = '123';
+            result = task7(context);
+            assert.isFalse(result);
+
+            context = { length : 10 };
+            result = task7(context);
+            assert.isOk(result);
+        });
+        it('function argument length min 1 max 47', () => {
+            let context = { length : 0 };
+            let result = task7(context);
+            assert.isFalse(result);
+
+            context = { length : 48};
+            result = task7(context);            
+            assert.isFalse(result);
+        });
+        it('function argument min > 0, max < 2000000000', () => {
+            let context = { min : 0, max : 10000 };
+            let result = task7(context);
+            assert.isFalse(result);
+
+            context = { min : 10, max : 2000000001 };
+            result = task7(context);
+            assert.isFalse(result);
+        });
+        it('function argument have min and max or length properties', () => {
+            let context = { notMin : 0, notMax : 10000 };
+            let result = task7(context);
+            assert.isFalse(result);
+
+            context = { notLength : 10 };
+            result = task7(context);
+            assert.isFalse(result);
+        });
     });
 }
